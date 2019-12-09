1e95f8f8c860ee65f06ec2effa93fb4853a9c3ec
Steve McDougall <juststeveking@gmail.com>
2019-12-09 10:45:57 +0000

    Editing purge css

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    whitelist: ['html', 'body', 'markdown'],
-    content: [
-      './src/**/*.vue',
-        './src/**/*.js',
-        './src/**/*.jsx',
-        './src/**/*.md'
-    ],
-    extractors: [{
-      extensions: ['vue', 'js', 'jsx', 'md'],
-      extractor: class TailwindExtractor {
-        static extract(content) {
-          return content.match(/[A-z0-9-:\/]+/g) || []
-        }
-      },
-    }],
-  }
-  
+  whitelist: ['html', 'body', 'markdown'],
+  content: [
+    './src/**/*.vue',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.md'
+  ],
+  extractors: [{
+    extensions: ['vue', 'js', 'jsx', 'md'],
+    extractor: class TailwindExtractor {
+      static extract(content) {
+        return content.match(/[\w-/:]+(?<!:)/g) || []
+      }
+    },
+  }],
+}
