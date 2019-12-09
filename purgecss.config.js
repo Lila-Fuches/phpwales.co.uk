89290c35445b7290d07f51b7f56880c90ee65eba
Steve McDougall <juststeveking@gmail.com>
2019-12-09 10:56:40 +0000

    Default purge css from tailwind site

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,17 +1,23 @@
-module.exports = {
-  whitelist: ['html', 'body', 'markdown'],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
   content: [
+    './src/**/*.html',
     './src/**/*.vue',
-    './src/**/*.js',
     './src/**/*.jsx',
-    './src/**/*.md'
+    // etc.
   ],
-  extractors: [{
-    extensions: ['vue', 'js', 'jsx', 'md'],
-    extractor: class TailwindExtractor {
-      static extract(content) {
-        return content.match(/[\w-/:]+(?<!:)/g) || []
-      }
-    },
-  }],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
 }
